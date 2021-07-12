0ae730d361e0a6e8f6e16e8dd38168697952a107
geonwoo.jeong <doscmdev@gmail.com>
2021-07-12 17:52:42 +0900

    fix: yarn build error

--- src/hooks/useDebounce.ts
+++ src/hooks/useDebounce.ts
@@ -1,24 +1,24 @@
-import { useEffect, useRef } from 'react';
+import { useRef } from 'react';
 
 export default function useDebounce<A extends unknown[]>(
   callback: (...args: A) => void,
   delay: number
 ) {
   const argsRef = useRef<A>();
-  const timeout = useRef<ReturnType<typeof setTimeout>>();
+  const timer = useRef<ReturnType<typeof setTimeout> | null>(null);
 
-  useEffect(() => {
-    return function cleanup() {
-      if (timeout.current) clearTimeout(timeout.current);
-    };
-  }, []);
+
+  function cleanup() {
+    if (timer.current) clearTimeout(timer.current);
+  };
 
   return function debouncedCallback(...args: A) {
     argsRef.current = args;
     cleanup();
-    timeout.current = setTimeout(() => {
+    timer.current = setTimeout(() => {
       if (argsRef.current) {
         callback(...argsRef.current);
+        timer.current = null;
       }
     }, delay);
   };
